d66942ef48c54db6ed16d865183eb331d3666cc9
Muhammad Waleed <muhammadwaleed.gpgc@gmail.com>
2026-08-07 10:33:39 +0500

    javascript code add another function in it

--- 6 Expense-Tracker App/script.js
+++ 6 Expense-Tracker App/script.js
@@ -8,7 +8,9 @@ const addTransactionBtn = document.getElementById('addTransactionBtn');
 const darkMode = document.getElementById('darkmode');
 const transactionTable = document.getElementById('transactionTableBody');
 
-const transactions = [];
+let editingId= null;
+
+let transactions = [];
 
 addTransactionBtn.addEventListener('click', addTransaction);
 
@@ -49,6 +51,9 @@ else if( type === ""){
     date :new Date(),
 
  }
+ if (editingId == null){
+
+ 
  transactions.push(transcation);
 
  saveToLocalStorage();
@@ -68,30 +73,115 @@ clearForm();
 
 
 toggleEmptyState();
+ } 
+ else {
+   const foundtransection = transactions.find(item => item.id === editingId);
+
+    foundtransection.title = title;
+    foundtransection.amount= amount;
+    foundtransection.category = category;
+    foundtransection.type = type;
+    editingId =null;
+    displayTransactions();
+    clearForm();
+   
+
+
+ }
 
 
 }
 
 function  displayTransactions(){
     transactionTable.innerHTML = "";
-    for (const transaction of transactions) {
+  transactions.forEach((transaction,index) => {
+    let display = index+1;
+
+
 
         const row = document.createElement('tr');
 
         row.innerHTML =`
-        <td>${transaction.id}</td> 
+        <td>${display}</td> 
         <td> ${transaction.title}</td>
         
         <td>${transaction.amount}</td>
         <td>${transaction.category}</td>
         <td>${transaction.type}</td> 
         <td>${transaction.date}</td> 
-        <td> <button>Edite </button>
-        <button>Delete</button> </td>
+        <td> <button class="editid">Edite </button>
+        <button class="deleteBtn">Delete</button> </td>
          `;
 
         transactionTable.appendChild(row);
+       
+
+        const edittransection = row.querySelector('.editid');
+        edittransection.addEventListener('click', ()=>{
+            editTransectionId(transaction.id);
+        })
+
+
+        const deleteBtn =row.querySelector('.deleteBtn');
+        deleteBtn.addEventListener('click',() =>{
+            deleteTransection(transaction.id);
+
+        })
+         });
 
 }
-}
 
+
+
+ function deleteTransection(id){
+    transactions = transactions.filter(item =>{
+        return item.id !== id;
+    })
+
+    displayTransactions();
+    updateDashboard();
+    saveToLocalStorage();
+
+toggleEmptyState();
+
+
+
+ }
+
+ function editTransectionId(id){
+    const finditem = transactions.find( item =>{
+      
+        return item.id === id;
+    });
+    titleInput.value = finditem.title;
+    amountInput.value = finditem.amount;
+    categorySelect.value = finditem.category;
+    typeSelect.value = finditem.type;
+    editingId = id;
+
+
+   
+
+
+
+
+
+
+ }
+
+ function clearForm(){
+     titleInput.value="";
+    amountInput.value = "";
+    categorySelect.value = "";
+    typeSelect.value = "";
+ }
+
+ function saveToLocalStorage(){
+
+ }
+ function updateDashboard(){
+
+ }
+ function toggleEmptyState(){
+
+ }
